9e3ebbce51b2855e6b15166267da6604fe0a2f22
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-04-27 21:40:14 +0300

    Update TriangularPrism.java

--- TriangularPrism.java
+++ TriangularPrism.java
@@ -10,6 +10,21 @@ public class TriangularPrism {
         this.side_c =side_c;
         this.side_h =side_h;
     }
+    public TriangularPrism(){}
+    public double areaTriangularPrism(double side_a, double side_b, double side_c, double side_h){
+        double p = (side_a+side_b+side_c)/2;//пів периметр
+        System.out.println(p);
+        double areaBaza =  Math.sqrt(p*((p - side_a)*(p-side_b)*(p-side_c)));//площа основи
+        System.out.println(areaBaza);
+        double areaLateral = (side_a+side_b+side_c)*side_h;// площа бічна
+        return  areaLateral+(2*areaBaza);
+    }
+    public void volumeTriangularPrism(double side_a, double side_b, double side_c, double side_h){
+        double p = (side_a+side_b+side_c)/2;//пів периметр
+        double areaBaza =  Math.sqrt(p*((p - side_a)*(p-side_b)*(p-side_c)));//площа основи
+        double volume = areaBaza *side_h;
+        System.out.println("Обєм = "+volume);
+    }
     public double areaTriangularPrism(){
         double p = (side_a+side_b+side_c)/2;//пів периметр
         System.out.println(p);
@@ -18,11 +33,6 @@ public class TriangularPrism {
         double areaLateral = (side_a+side_b+side_c)*side_h;// площа бічна
         return  areaLateral+(2*areaBaza);
     }
-//    public double volumeTriangularPrism(){
-//        double p = (side_a+side_b+side_c)/2;//пів периметр
-//        double areaBaza =  Math.sqrt(p*((p - side_a)*(p-side_b)*(p-side_c)));//площа основи
-//        return areaBaza *side_h;
-//    }
     public void volumeTriangularPrism(){
         double p = (side_a+side_b+side_c)/2;//пів периметр
         double areaBaza =  Math.sqrt(p*((p - side_a)*(p-side_b)*(p-side_c)));//площа основи
